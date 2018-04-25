3a9cfb077cec91b65007ec672d7c036cc7b2b873
rtrocoletto <38508391+rtrocoletto@users.noreply.github.com>
2018-04-25 16:11:12 -0300

    Add files via upload

---
+++ Prototipo - Efinanceira/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtXML = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtChave = new System.Windows.Forms.TextBox();
+            this.txtLote = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtXML
+            // 
+            this.txtXML.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtXML.Location = new System.Drawing.Point(13, 13);
+            this.txtXML.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtXML.Multiline = true;
+            this.txtXML.Name = "txtXML";
+            this.txtXML.Size = new System.Drawing.Size(820, 105);
+            this.txtXML.TabIndex = 1;
+            this.txtXML.Text = "<loteEventos>";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Criptografar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtChave
+            // 
+            this.txtChave.Location = new System.Drawing.Point(13, 199);
+            this.txtChave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtChave.Multiline = true;
+            this.txtChave.Name = "txtChave";
+            this.txtChave.Size = new System.Drawing.Size(820, 133);
+            this.txtChave.TabIndex = 3;
+            // 
+            // txtLote
+            // 
+            this.txtLote.Location = new System.Drawing.Point(13, 356);
+            this.txtLote.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtLote.Multiline = true;
+            this.txtLote.Name = "txtLote";
+            this.txtLote.Size = new System.Drawing.Size(820, 133);
+            this.txtLote.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 179);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Chave";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 336);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Lote";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 502);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLote);
+            this.Controls.Add(this.txtChave);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtXML);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Protótipo para Criptografia de dados - E-Financeira";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtXML;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtChave;
+        private System.Windows.Forms.TextBox txtLote;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Prototipo - Efinanceira/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtXML.Text = "<loteEventos><evento id = 'ID777'><tagTeste>Teste</TagTeste></evento></loteEventos>";
+        }
+
+        public byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+        private KeyValuePair<byte[], byte[]> EncryptStringToBytes_Aes(string plainText, byte[] Key, ref byte[] IV)
+        {
+            byte[] encrypted;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.GenerateIV();
+                IV = aesAlg.IV;
+                aesAlg.Mode = CipherMode.CBC;
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            var combinedKeyIv = new byte[Key.Length + IV.Length];
+            Array.Copy(Key, 0, combinedKeyIv, 0, Key.Length);
+            Array.Copy(IV, 0, combinedKeyIv, Key.Length, IV.Length);
+            return new KeyValuePair<byte[], byte[]>(combinedKeyIv, encrypted);      
+
+        }
+
+        public static byte[] EncriptarPrivateKeyToRSA(byte[] privateKey, X509Certificate2 certificado)
+        {
+            using (var cryptoProvider = (RSACryptoServiceProvider)certificado.PublicKey.Key)
+            {
+                //cryptoProvider
+                return cryptoProvider.Encrypt(privateKey, false);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //cria a chave randomica de 16bytes
+            RNGCryptoServiceProvider random = new RNGCryptoServiceProvider();
+            byte[] key = new byte[16];
+            random.GetBytes(key);
+
+            byte[] retIV = null; //irá retornar byRef na chamada da função            
+
+            //passa o XML e a chave randomica como parametro
+            //retorno
+            //retornoLoteEncriptado.Key   ==> Chave (Key + IV)
+            //retornoLoteEncriptado.Value ==> Lote Criptografado
+            KeyValuePair<byte[], byte[]> retornoLoteEncriptado = EncryptStringToBytes_Aes(txtXML.Text, key, ref retIV);
+
+            //converte para base64 e define conteúdo da tag LOTE
+            string lote = Convert.ToBase64String(retornoLoteEncriptado.Value);
+            txtLote.Text = lote;
+
+            //instancia o certificado de envio
+            //copiar o certificado de preprod para C:\Temp\
+            X509Certificate2 cert = new X509Certificate2("C:\\temp\\preprod-efinancentreposto.receita.fazenda.gov.br.cer");
+
+            //chave utilizada para descriptografar o xml enviado para a receita
+            byte[] tagChave = EncriptarPrivateKeyToRSA(retornoLoteEncriptado.Key, cert);
+
+            //converte para base64 e define conteúdo da tag CHAVE
+            string chave = Convert.ToBase64String(tagChave);
+            txtChave.Text = chave;
+            
+        }
+
+    }
+}
